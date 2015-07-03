9562fe2a63ad425c57ae3ed0fb2293d4a1ba7051
xiaofan <xiaofan@kasual.biz>
2015-07-03 14:35:42 +0200

    add alias

--- alias.zsh
+++ alias.zsh
@@ -1,11 +1,9 @@
+# Custom directory access
 # Add symbolic link to home first
 todo=/Users/wuxxf/todo
 dropbox=/Users/wuxxf/Dropbox
 googledrive=/Users/wuxxf/Google\ Drive
 
-# others
-alias cl="clear"
-alias vim="mvim"
 # pow
 alias pow_restart="touch ~/tmp/restart.txt"
 
@@ -14,3 +12,11 @@ alias pysvr="python -m SimpleHTTPServer 8000"
 alias rubysvr="ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd).start'" 
 alias phpsvr0="php -S localhost:8000" 
 alias phpsvr1="php -S localhost:8001"
+
+# git
+alias gim "git ci -m"
+
+# others
+alias cl="clear"
+alias vim="mvim"
+
