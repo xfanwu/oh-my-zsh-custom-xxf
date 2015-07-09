ab1b90e3937dc275108eb7b3b9aee096469decb8
Xiaofan Wu <qvanwu@gmail.com>
2015-07-09 22:06:07 +0200

    add alias

--- alias.zsh
+++ alias.zsh
@@ -1,4 +1,5 @@
-# Add symbolic link to home first
+#
+#Add symbolic link to home first
 todo=/Users/wuxxf/todo
 dropbox=/Users/wuxxf/Dropbox
 googledrive=/Users/wuxxf/Google\ Drive
@@ -6,11 +7,12 @@ googledrive=/Users/wuxxf/Google\ Drive
 # others
 alias cl="clear"
 alias vim="mvim"
+
 # pow
-alias pow_restart="touch ~/tmp/restart.txt"
+alias pow_restart="touch ~/.pow/restart.txt"
 
 # built-in servers
 alias pysvr="python -m SimpleHTTPServer 8000" 
-alias rubysvr="ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd).start'" 
-alias phpsvr0="php -S localhost:8000" 
-alias phpsvr1="php -S localhost:8001"
+alias rubysvr="ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8002 :DocumentRoot => Dir.pwd).start'" 
+alias phpsvr="php -S localhost:8001"
+alias jksvr="jekyll serve --watch"
