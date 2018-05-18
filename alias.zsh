7357b0160424c6643d7314ff69d81261c6d8496a
Xiaofan Wu <xfanwu@gmail.com>
2018-05-18 11:54:13 +0800

    update alias

--- alias.zsh
+++ alias.zsh
@@ -7,17 +7,22 @@
 # built-in servers
 alias pysvr="python -m SimpleHTTPServer 8000" 
 alias rubysvr="ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 3001, :DocumentRoot => Dir.pwd).start'" 
-alias phpsvr1="php -S localhost:8001"
 alias nodesvr="hs -p 8000"
 
 # git
-alias gc="git commit"
-alias gimc="git ci --amend -m"
-alias giv="git commit --verbose"
+alias gco="git checkout"
+alias gcm="git commit"
+alias gcma="git ci --amend -m"
+alias gpl="git pull"
+alias gps="git push"
+
+# docker
+alias docker-rm-exited='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
 
 # others
 alias cl="clear"
 alias vim="mvim"
 alias rake="noglob rake"
-alias sourcezshrc="source ~/.zshrc"
+alias srczshrc="source ~/.zshrc"
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+
